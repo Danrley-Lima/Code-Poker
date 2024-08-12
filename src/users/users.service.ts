3be26b606e4d57f7c6f1556dc61f92ad00ec2a04
Danrley Lima <danrleylac@gmail.com>
2024-08-11 22:50:43 -0300

    feat: adiciona lógica para cadastrar usuário como GUEST ou PARTICIPANT

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserRole } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const isGuest = !createUserDto.email && !createUserDto.password;
+    const role = isGuest ? UserRole.GUEST : UserRole.PARTICIPANT;
+
+    if (!isGuest) {
+      const existingUser = await this.prisma.user.findUnique({
+        where: {
+          email: createUserDto.email,
+        },
+      });
+
+      if (existingUser) {
+        throw new Error('Email already exists');
+      }
+    }
+
     return this.prisma.user.create({
-      data: createUserDto,
+      data: { ...createUserDto, role },
     });
   }
 
